b1dafe45c13a7a0381e767ca75e53a5fe850bb64
suntao <mrls@live.cn>
2016-07-17 21:09:07 +0800

    修改反向代理

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 // 设置代理
-app.set('trust proxy', 'true');
+app.enable("trust proxy")
+app.set('trust proxy', 'loopback');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
