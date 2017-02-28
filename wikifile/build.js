1ed47dfa32db4dc3e98db20bedc9e066860bd6e0
Theo Sun <theo.sun@sap.com>
2017-02-28 13:05:37 +0800

    use html5 cache rather than service worker

--- wikifile/build.js
+++ wikifile/build.js
@@ -13,12 +13,21 @@ const getDirsFromDir = node => _.filter(node.children, item => !item.extension &
  */
 const upper = str => capitalize.words(str)
 
-const writeLine = (line) => {
+const writeNavigationLine = (line) => {
   if (fs.existsSync(targetNavigationFilePath))
     fs.appendFileSync(targetNavigationFilePath, (line || "") + os.EOL)
   else
     fs.writeFileSync(targetNavigationFilePath, (line || "") + os.EOL);
 }
+
+const writeAppcacheLine = (line) => {
+  if (fs.existsSync(targetAppcacheFilePath))
+    fs.appendFileSync(targetAppcacheFilePath, (line || "") + os.EOL)
+  else
+    fs.writeFileSync(targetAppcacheFilePath, (line || "") + os.EOL);
+}
+
+
 
 /**
  * make a markdown format link
@@ -51,214 +60,71 @@ const getMdTitle = filepath => {
 
 const writeNavigationFile = () => {
   // start write new navigation
-  writeLine("# wiki")
-  writeLine()
-  getFilesFromDir(tree).forEach(item => item.name == 'navigation.md' ? writeLine() : writeLine(makeLink(item)) & writeLine())
+  writeNavigationLine("# wiki")
+  writeNavigationLine()
+  getFilesFromDir(tree).forEach(item => item.name == 'navigation.md' ? writeNavigationLine() : writeNavigationLine(makeLink(item)) & writeNavigationLine())
   getDirsFromDir(tree).forEach((dir) => {
     // if a directory not have any file, will be skiped
     if (!dir.extension && dir.children.length < 1)
       return
-    writeLine(makeLink(dir)) & writeLine()
-    getFilesFromDir(dir).forEach(article => writeLine(`* ${makeLink(article)}`))
-    getDirsFromDir(dir).forEach((dir, idx, arr) => {
-      writeLine(`* # ${upper(dir.name)}`)
-      getFilesFromDir(dir).forEach(article => writeLine(`* ${makeLink(article)}`))
-      if (arr.length - 1 != idx) writeLine('---')
+    writeNavigationLine(makeLink(dir)) & writeNavigationLine()
+    getFilesFromDir(dir).forEach(article => {
+      writeNavigationLine(`* ${makeLink(article)}`)
     })
-    writeLine()
+    getDirsFromDir(dir).forEach((dir, idx, arr) => {
+      writeNavigationLine(`* # ${upper(dir.name)}`)
+      getFilesFromDir(dir).forEach(article => writeNavigationLine(`* ${makeLink(article)}`))
+      if (arr.length - 1 != idx) writeNavigationLine('---')
+    })
+    writeNavigationLine()
   })
 
   console.log('navigation generate finished')
 }
 
-
-
-const sw_str = `'use strict';
-
-const version = "${(new Date()).toLocaleString()}";
-const __DEVELOPMENT__ = false;
-const __DEBUG__ = false;
-const offlineResources = [
-];
-
-const ignoreFetch = [
-];
-
-
-//////////
-// Install
-//////////
-function onInstall(event) {
-  log('install event in progress.');
-  self.skipWaiting();
-  event.waitUntil(updateStaticCache());
-}
-
-function updateStaticCache() {
-  return caches
-    .open(cacheKey('offline'))
-    .then((cache) => {
-      return cache.addAll(offlineResources);
-    })
-    .then(() => {
-      log('installation complete!');
-    });
-}
-
-////////
-// Fetch
-////////
-function onFetch(event) {
-  const request = event.request;
-
-  if (shouldAlwaysFetch(request)) {
-    event.respondWith(networkedOrOffline(request));
-    return;
+const get_path_from = (article) => {
+  if (article) {
+    return article.path.replace(/\\/g, '/')
   }
-
-  if (shouldFetchAndCache(request)) {
-    event.respondWith(networkedOrCached(request));
-    return;
-  }
-
-  event.respondWith(cachedOrNetworked(request));
+  return "";
 }
 
-function networkedOrCached(request) {
-  return networkedAndCache(request)
-    .catch(() => { return cachedOrOffline(request) });
+const writeAppcacheFile = () => {
+  // start write new appcache file
+  writeAppcacheLine("CACHE MANIFEST");
+  writeAppcacheLine(`# version ${new Date()}`);
+  writeAppcacheLine('config.json');
+  
+  getFilesFromDir(tree).forEach(article => writeAppcacheLine(get_path_from(article)))
+  getDirsFromDir(tree).forEach((dir) => {
+    // if a directory not have any file, will be skiped
+    if (!dir.extension && dir.children.length < 1)
+      return
+    getFilesFromDir(dir).forEach(article => writeAppcacheLine(get_path_from(article)))
+    getDirsFromDir(dir).forEach((dir, idx, arr) => getFilesFromDir(dir).forEach(article => writeAppcacheLine(get_path_from(article))))
+  })
+  writeAppcacheLine()
+  writeAppcacheLine('NETWORK:');
+  writeAppcacheLine('*')
+  console.log('appcache generate finished')
 }
-
-// Stash response in cache as side-effect of network request
-function networkedAndCache(request) {
-  return fetch(request)
-    .then((response) => {
-      var copy = response.clone();
-      caches.open(cacheKey('resources'))
-        .then((cache) => {
-          cache.put(request, copy);
-        });
-
-      log("(network: cache write)", request.method, request.url);
-      return response;
-    });
-}
-
-function cachedOrNetworked(request) {
-  return caches.match(request)
-    .then((response) => {
-      log(response ? '(cached)' : '(network: cache miss)', request.method, request.url);
-      return response ||
-        networkedAndCache(request)
-          .catch(() => { return offlineResponse(request) });
-    });
-}
-
-function networkedOrOffline(request) {
-  return fetch(request)
-    .then((response) => {
-      log('(network)', request.method, request.url);
-      return response;
-    })
-    .catch(() => {
-      return offlineResponse(request);
-    });
-}
-
-function cachedOrOffline(request) {
-  return caches
-    .match(request)
-    .then((response) => {
-      return response || offlineResponse(request);
-    });
-}
-
-function offlineResponse(request) {
-  log('(offline)', request.method, request.url);
-  if (request.url.match(/\.(jpg|png|gif|svg|jpeg)(\?.*)?$/)) {
-    return caches.match('/offline.svg');
-  } else {
-    return caches.match('/offline.html');
-  }
-}
-
-///////////
-// Activate
-///////////
-function onActivate(event) {
-  log('activate event in progress.');
-  event.waitUntil(removeOldCache());
-}
-
-function removeOldCache() {
-  return caches
-    .keys()
-    .then((keys) => {
-      return Promise.all( // We return a promise that settles when all outdated caches are deleted.
-        keys
-         .filter((key) => {
-           return !key.startsWith(version); // Filter by keys that don't start with the latest version prefix.
-         })
-         .map((key) => {
-           return caches.delete(key); // Return a promise that's fulfilled when each outdated cache is deleted.
-         })
-      );
-    })
-    .then(() => {
-      log('removeOldCache completed.');
-    });
-}
-
-function cacheKey() {
-  return [version, ...arguments].join(':');
-}
-
-function log() {
-  if (developmentMode()) {
-    console.log("SW:", ...arguments);
-  }
-}
-
-function shouldAlwaysFetch(request) {
-  return __DEVELOPMENT__ ||
-    request.method !== 'GET' ||
-      ignoreFetch.some(regex => request.url.match(regex));
-}
-
-function shouldFetchAndCache(request) {
-  return ~request.headers.get('Accept').indexOf('text/html');
-}
-
-function developmentMode() {
-  return __DEVELOPMENT__ || __DEBUG__;
-}
-
-log("Hello from ServiceWorker land!", version);
-
-self.addEventListener('install', onInstall);
-
-self.addEventListener('fetch', onFetch);
-
-self.addEventListener("activate", onActivate);
-`
 
 const tree = dirTree('.', '.md')
 const targetNavigationFilePath = 'navigation.md';
-const targetSWFilePath = 'sw.js'
+const targetAppcacheFilePath = 'wiki.appcache';
 
 if (fs.existsSync(targetNavigationFilePath)) {
   fs.unlinkSync(targetNavigationFilePath);
   console.log('deleted old navigation');
 }
 
-writeNavigationFile()
-
-if (fs.existsSync(targetSWFilePath)) {
-  fs.unlinkSync(targetSWFilePath);
-  console.log('deleted old sw.js')
+if (fs.existsSync(targetAppcacheFilePath)) {
+  fs.unlinkSync(targetAppcacheFilePath);
+  console.log('deleted old navigation');
 }
 
-fs.writeFileSync(targetSWFilePath, sw_str);
-console.log('regenerate sw.js')
+
+writeNavigationFile()
+writeAppcacheFile()
 
 console.log('build success!')
